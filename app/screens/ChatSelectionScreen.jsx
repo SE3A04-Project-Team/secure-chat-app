6a83570d0730539d9de09c018ca7d7eb8d0921b0
Aidan Froggatt <aidanfr@live.ca>
2024-04-03 00:50:17 -0400

    chat selection ui has dates and updated style

--- app/screens/ChatSelectionScreen.jsx
+++ app/screens/ChatSelectionScreen.jsx
@@ -4,7 +4,7 @@ import IconButton from "../components/IconButton";
 
 const ChatSelectionScreen = ({ navigation }) => {
 
-    // TODO: Query chat data (only name and id) from backend
+    // TODO: Query chat data from backend
 
     // Sample JSON data for chat selection
     const chatData = [
@@ -12,76 +12,91 @@ const ChatSelectionScreen = ({ navigation }) => {
             id: 1,
             name: 'John Doe',
             timeOfLastMessage: '2024-04-03T18:25:43.511Z', // ISO 8601 format
+            lastMessage: 'Hello, how are you?',
         },
         {
             id: 2,
             name: 'Jane Doe',
             timeOfLastMessage: '2024-02-21T14:25:43.511Z',
+            lastMessage: 'I am good, thank you!',
         },
         {
             id: 3,
             name: 'Alice',
             timeOfLastMessage: '2024-04-02T13:25:43.511Z',
+            lastMessage: 'That\'s great to hear!',
         },
         {
             id: 4,
             name: 'Bob',
             timeOfLastMessage: '2024-03-27T19:25:43.511Z',
+            lastMessage: 'How are you doing?',
         },
         {
             id: 5,
             name: 'Charlie',
             timeOfLastMessage: '2024-03-27T17:25:43.511Z',
+            lastMessage: 'I am doing well, thank you!',
         },
         {
             id: 6,
             name: 'David',
             timeOfLastMessage: '2024-03-27T15:25:43.511Z',
+            lastMessage: 'Good to hear!',
         },
         {
             id: 7,
             name: 'Eve',
             timeOfLastMessage: '2024-03-27T16:25:43.511Z',
+            lastMessage: 'Hi there!',
         },
         {
             id: 8,
             name: 'Frank',
             timeOfLastMessage: '2024-03-27T12:25:43.511Z',
+            lastMessage: 'Hello!',
         },
         {
             id: 9,
             name: 'Grace',
             timeOfLastMessage: '2024-03-27T11:25:43.511Z',
+            lastMessage: 'Hey!',
         },
         {
             id: 10,
             name: 'Henry',
             timeOfLastMessage: '2024-03-27T10:25:43.511Z',
+            lastMessage: 'Howdy!',
         },
         {
             id: 11,
             name: 'Ivy',
             timeOfLastMessage: '2024-03-27T09:25:43.511Z',
+            lastMessage: 'Hi!',
         },
         {
             id: 12,
             name: 'Jack',
             timeOfLastMessage: '2024-03-27T08:25:43.511Z',
+            lastMessage: 'Hey there!',
         },
         {
             id: 13,
             name: 'Katie',
             timeOfLastMessage: '2024-03-27T07:25:43.511Z',
+            lastMessage: 'Hello, how are you?',
         },
         {
             id: 14,
             name: 'Liam',
             timeOfLastMessage: '2024-04-02T06:25:43.511Z',
+            lastMessage: 'I am good, thank you!',
         },
         {
             id: 15,
             name: 'Mia',
             timeOfLastMessage: '2024-04-01T05:25:43.511Z',
+            lastMessage: 'That\'s great to hear!',
         }
     ];
 
@@ -149,13 +164,18 @@ const ChatSelectionScreen = ({ navigation }) => {
                                 const formattedDate = formatDate(chat.timeOfLastMessage);
 
                                 return (
-                                <TouchableOpacity key={chat.id} onPress={() => handleChatSelection(chat)} className="flex flex-row w-full p-4 content-center items-center">
-                                    <View className="w-10 h-10 rounded-full flex justify-center items-center bg-black">
+                                <TouchableOpacity key={chat.id} onPress={() => handleChatSelection(chat)} className="flex flex-row w-full p-4 content-center items-center justify-center">
+                                    <View className="w-10 h-10 rounded-full flex justify-center items-center bg-gray-400">
                                         <Text className="text-xl font-bold text-primary">{getInitials(chat.name)}</Text>
                                     </View>
-                                    <Text className="flex-grow text-lg mx-2">{chat.name}</Text>
-                                    <Text>{formattedDate}</Text>
-                                    <Icon name="angle-right" size={24} color="black" className="self-end"/>
+                                    <View className="flex flex-col flex-grow">
+                                        <View className="flex flex-row justify-between items-center content-center mx-2">
+                                            <Text className="text-lg flex-grow font-semibold">{chat.name}</Text>
+                                            <Text className="px-2 text-gray-400">{formattedDate}</Text>
+                                            <Icon name="angle-right" size={24} color="#9ca3af" className="self-end"/>
+                                        </View>
+                                        <Text className="mx-2 text-gray-400">{chat.lastMessage}</Text>
+                                    </View>
                                 </TouchableOpacity>
                                 );
                             })
