7f98e261ef9b9ae980f45a7d9b9569b792281291
Aidan Froggatt <aidanfr@live.ca>
2024-04-07 15:00:29 -0400

    updated text button styling

--- frontend-user/components/TextButton.jsx
+++ frontend-user/components/TextButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 
-const TextButton = ({ onPress, title }) => {
+const TextButton = ({ onPress, title, opacityStyle, textStyle }) => {
 
     return (
-        <TouchableOpacity onPress={onPress} className="py-3 bg-secondary rounded-xl">
-            <Text className="text-xl font-bold text-center text-primary">{title}</Text>
+        <TouchableOpacity onPress={onPress} className={opacityStyle ? opacityStyle : 'py-3 bg-secondary rounded-xl'}>
+            <Text className={textStyle ? textStyle : 'text-xl font-bold text-center text-primary'}>{title}</Text>
         </TouchableOpacity>
     );
 };
