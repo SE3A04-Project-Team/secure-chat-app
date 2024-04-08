966e1b431d1c3689b408fb4503f6b60a7134e436
Aidan Froggatt <aidanfr@live.ca>
2024-04-08 11:37:06 -0400

    updated initial icon to only first two initials of chat name

--- frontend-user/components/InitialIcon.jsx
+++ frontend-user/components/InitialIcon.jsx
@@ -1,11 +1,11 @@
 import {Text, View} from "react-native";
-import {getInitials} from "../utils/stringUtils";
+import {getFirstTwoInitials} from "../utils/stringUtils";
 
 const InitialIcon = ({ name }) => {
 
     return (
         <View className="w-10 h-10 rounded-full flex justify-center items-center bg-gray-400">
-            <Text className="text-xl font-bold text-primary">{getInitials(name)}</Text>
+            <Text className="text-xl font-bold text-primary">{getFirstTwoInitials(name)}</Text>
         </View>
     );
 };
--- frontend-user/utils/stringUtils.js
+++ frontend-user/utils/stringUtils.js
@@ -6,5 +6,15 @@ const getInitials = (name) => {
     return nameArray.map(word => word[0]).join("").toUpperCase();
 };
 
+// Get the first two initials from a name
+const getFirstTwoInitials = (name) => {
+    return name
+        .split(" ") // Split the name into an array of words
+        .slice(0, 2) // Take only the first two words
+        .map(word => word[0]) // Get the first letter of each word
+        .join("") // Concatenate the letters into a string
+        .toUpperCase(); // Convert to uppercase
+};
+
 // Export the getInitials function
-export {getInitials};
+export {getInitials, getFirstTwoInitials};
